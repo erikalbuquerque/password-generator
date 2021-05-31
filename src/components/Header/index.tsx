88a858e5ad45e0af1e6828021368e10910402872
Érik Albuquerque <eriksilv.77@gmai.com>
2021-05-31 20:27:44 -0300

    feat: add indicator to light and dark theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import styles from "./style.module.scss"
 import { RiShieldKeyholeFill } from "react-icons/ri";
 import { BsToggleOff, BsToggleOn } from "react-icons/bs"
+import { FaMoon, FaRegMoon } from "react-icons/fa"
+import { HiSun, HiOutlineSun } from "react-icons/hi"
 import { useTheme } from "../../contexts/themeContext";
 
 export function Header() {
@@ -10,14 +12,23 @@ export function Header() {
   return (
     <div className={styles.container}>
       <div className={`${styles.logo} ${darkMode}`}>
-        <h1>Password <RiShieldKeyholeFill size={25} />  Generator</h1>
+        <h1>Password <RiShieldKeyholeFill size={25} /> Generator</h1>
       </div>
 
       <div className={`${styles.settings} ${darkMode}`}>
-        <span>Dark Theme</span>
         {darkTheme
-          ? (<BsToggleOn onClick={setTheme} size={20} />)
-          : (<BsToggleOff onClick={setTheme} size={20} />)}
+          ? <HiOutlineSun size={15} />
+          : <HiSun size={15} />
+        }
+        {darkTheme
+          ? <BsToggleOn onClick={setTheme} size={20} />
+          : <BsToggleOff onClick={setTheme} size={20} />
+        }
+        {darkTheme
+          ? <FaMoon size={12} />
+          : <FaRegMoon size={12} />
+        }
+
       </div>
 
     </div>
