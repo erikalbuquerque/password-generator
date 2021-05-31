b133d7b356fcc80ab5d8d76a6e7c588f2f3a2157
Érik Albuquerque <eriksilv.77@gmai.com>
2021-05-31 16:51:56 -0300

    feat: add change theme button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import styles from "./style.module.scss"
 import { RiShieldKeyholeFill } from "react-icons/ri";
+import { BsToggleOff, BsToggleOn } from "react-icons/bs"
 import { useContext } from "react";
 import { ThemeContext } from "../../contexts/themeContext";
 
 export function Header() {
-  const { darkTheme } = useContext(ThemeContext)
+  const { darkTheme, setTheme } = useContext(ThemeContext)
 
   const darkMode = darkTheme && styles.dark;
   return (
     <div className={styles.container}>
       <div className={`${styles.logo} ${darkMode}`}>
         <h1>Password <RiShieldKeyholeFill size={25} color={`${darkTheme ? "var(--text-white)" : "var(--text-black)"}`} /> Generator</h1>
+      </div>
+
+      <div className={`${styles.settings} ${darkMode}`}>
+        <span>Theme</span>
+        {darkTheme
+          ? (<BsToggleOn onClick={setTheme} size={20} color={`${darkTheme ? "var(--text-white)" : "var(--text-black)"}`} />)
+          : (<BsToggleOff onClick={setTheme} size={20} color={`${darkTheme ? "var(--text-white)" : "var(--text-black)"}`} />)}
       </div>
 
     </div>
